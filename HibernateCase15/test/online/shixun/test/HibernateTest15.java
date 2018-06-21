962e9cdc3b8c9e9734cdbefed36ff79e180df88f
yuanyixiong <15926499574@163.com>
2018-06-21 15:29:59 +0800

    Hibernate 4.3.11 使用Hibernate JPA 探讨Criteria条件查询

---
+++ HibernateCase15/test/online/shixun/test/HibernateTest15.java
@@ -0,0 +1,82 @@
+package online.shixun.test;
+
+import java.util.Date;
+import java.util.List;
+
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.criterion.Restrictions;
+import org.hibernate.service.ServiceRegistry;
+
+import online.shixun.model.Commodity;
+
+
+public class HibernateTest15 {
+	SessionFactory sessionFactory;
+	Session session;
+	Transaction transaction;
+
+	/**
+	 * 完成hibernate初始化
+	 */
+	public void init() {
+		// 获取配置文件
+		Configuration configuration = new Configuration().configure("hibernate.cfg.xml");
+		// 注册服务对象
+		StandardServiceRegistryBuilder ssrb = new StandardServiceRegistryBuilder();
+		ServiceRegistry serviceRegistry = ssrb.applySettings(configuration.getProperties()).build();
+		// 获取sessionfactory
+		sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+		// 获取Session
+		session = sessionFactory.openSession();
+		// 开启事物
+		transaction = session.beginTransaction();
+	}
+
+	/**
+	 * 销毁hibernate资源
+	 */
+	public void destroy() {
+		// 提交事物
+		transaction.commit();
+		// 一级缓存
+		session.close();
+		// 二级缓存
+		sessionFactory.close();
+	}
+
+	/**
+	 * 添加
+	 */
+	public void save() {
+		init();
+		session.save(new Commodity("滑板", "便利的交通工具", new Date(), new Date()));
+		session.save(new Commodity("毛巾", "洗脸", new Date(), new Date()));
+		session.save(new Commodity("脸盆", "洗脸", new Date(), new Date()));
+		session.save(new Commodity("肥皂", "洗澡", new Date(), new Date()));
+		session.save(new Commodity("自行车", "便利的交通工具", new Date(), new Date()));
+		destroy();
+	}
+
+	public void getCommoditys() {
+		Criteria criteria=session.createCriteria(Commodity.class);
+		criteria.add(Restrictions.like("name", "%乐事%"));
+		criteria.add(Restrictions.le("id", 5000));
+		List<Commodity> list = criteria.list();
+		for (Commodity commodity : list) {
+			System.out.println(commodity.toString());
+		}
+	}
+
+	public static void main(String[] args) {
+		HibernateTest15 hibernateTest09 = new HibernateTest15();
+		hibernateTest09.init();
+		hibernateTest09.getCommoditys();
+		hibernateTest09.destroy();
+	}
+
+}
